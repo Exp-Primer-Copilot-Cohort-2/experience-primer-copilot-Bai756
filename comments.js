a8d6cdc4d6e3f9b0cbf6bd099891e14174955dbc
En Qiao <147208730+Bai756@users.noreply.github.com>
2024-08-20 21:24:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,88 +1,35 @@
-// create web server
-// use express.js
-// use body-parser
-// use path
-// use mysql
-// use ejs
-// use express-session
-// use express-validator
+// Create a web server
+// The server should listen on port 3000
+// The server should respond to a GET request to '/hello' with a web page that says "Hello, World!"
+// The server should respond to a GET request to '/goodbye' with a web page that says "Goodbye, World!"
 
-// create connection to database
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'test'
+// Import the http module
+var http = require('http');
+// Import the url module
+var url = require('url');
+
+// Create a server
+var server = http.createServer(function (req, res) {
+  // Parse the request URL
+  var parsedUrl = url.parse(req.url, true);
+
+  // Check the path of the URL
+  if (parsedUrl.pathname === '/hello') {
+    // Send a response with the text "Hello, World!"
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, World!</h1>');
+  } else if (parsedUrl.pathname === '/goodbye') {
+    // Send a response with the text "Goodbye, World!"
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Goodbye, World!</h1>');
+  } else {
+    // Send a response with the text "Not Found"
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Not Found</h1>');
+  }
 });
 
-// connect to database
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Connected to database');
-});
-
-// create express app
-var express = require('express');
-var app = express();
-
-// set view engine to ejs
-app.set('view engine', 'ejs');
-
-// use body-parser
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// use express-session
-var session = require('express-session');
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
-
-// use express-validator
-var expressValidator = require('express-validator');
-app.use(expressValidator());
-
-// create route for home page
-app.get('/', function(req, res) {
-    res.render('index');
-});
-
-// create route for login page
-app.get('/login', function(req, res) {
-    res.render('login');
-});
-
-// create route for signup page
-app.get('/signup', function(req, res) {
-    res.render('signup');
-});
-
-// create route for login request
-app.post('/login', function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], function(err, result) {
-        if (result.length > 0) {
-            req.session.loggedin = true;
-            req.session.email = email;
-            res.redirect('/');
-        } else {
-            res.send('Incorrect email or password');
-        }
-        res.end();
-    });
-});
-
-// create route for signup request
-app.post('/signup', function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    var confirmPassword = req.body.confirmPassword;
-    req.checkBody('email', 'Email is required').notEmpty();
-    req.checkBody('email', 'Email is not valid').isEmail();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('confirmPassword', '');
+// Start the server
+server.listen(3000, function () {
+  console.log('Server is listening on port 3000');
 });
